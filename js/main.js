af53c104ed2ee814253dabe95bc8b2d187276560
Chung Jason <shiningjason1989@gmail.com>
2015-09-11 01:11:22 +0800

    Remove main.js

--- js/main.js
+++
@@ -1,6 +0,0 @@
-require('../assets/stylesheets/style.sass');
-
-import React from 'react';
-import AppContainer from './components/AppContainer';
-
-React.render(<AppContainer />, document.getElementById('main'));
